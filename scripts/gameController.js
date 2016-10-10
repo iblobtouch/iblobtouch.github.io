ddf98ecee277e7de5d02d890164247286a5f7c3c
Acarii <evetekacarii@gmail.com>
2016-10-09 20:13:09 -0400

    Tweaked setcolor functions
    As I suspected, if statements were not needed. Yay learning!

--- scripts/gameController.js
+++ scripts/gameController.js
@@ -912,46 +912,33 @@ function redo() {
 	}
 }
 
+//Set colour functions. Used in presets
 function setcolorred() {
-	if (document.getElementById("color").value != "#F14E54") {
-		document.getElementById("color").value = "#F14E54";
-	}
+	document.getElementById("color").value = "#F14E54";
 }
 
 function setcolorblue() {
-	if (document.getElementById("color").value != "#00B2E1") {
-		document.getElementById("color").value = "#00B2E1";
-	}
+	document.getElementById("color").value = "#00B2E1";
 }
 
 function setcolorgreen() {
-	if (document.getElementById("color").value != "#00E16E") {
-		document.getElementById("color").value = "#00E16E";
-	}
+	document.getElementById("color").value = "#00E16E";
 }
 
 function setcolorpurple() {
-	if (document.getElementById("color").value != "#BF7FF5") {
-		document.getElementById("color").value = "#BF7FF5";
-	}
+	document.getElementById("color").value = "#BF7FF5";
 }
 
 function setcoloryellow() {
-	if (document.getElementById("color").value != "#FFEB69") {
-		document.getElementById("color").value = "#FFEB69";
-	}
+	document.getElementById("color").value = "#FFEB69";
 }
 
 function setcolorlightgrey() {
-	if (document.getElementById("color").value != "#C0C0C0") {
-		document.getElementById("color").value = "#C0C0C0";
-	}
+	document.getElementById("color").value = "#C0C0C0";
 }
 
 function setcolorpalered() {
-	if (document.getElementById("color").value != "#FC7677") {
-		document.getElementById("color").value = "#FC7677";
-	}
+	document.getElementById("color").value = "#FC7677";
 }
 
 function setcolorviolet() {
